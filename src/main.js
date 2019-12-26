0f0e2df06bd10b4de90ffdae41561778ce1c7cbc
leejaeho <jhlee@ibleaders.co.kr>
2019-12-26 16:23:08 +0900

    약간의 수정 커밋

--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
 window.onload = function() {
     alert("안녕하세요");
+    var jbBtn = document.createElement( 'button' );
+    var jbBtnText = document.createTextNode( 'Click' );
+    jbBtn.appendChild( jbBtnText );
+    document.body.appendChild( jbBtn );
 }
